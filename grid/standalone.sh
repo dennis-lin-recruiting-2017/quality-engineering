8214ebf32cc955c8de105a4f4dc66b14ae333557
Dennis Lin <dennis.lin.recruiting.2017@gmail.com>
2020-04-15 14:00:27 -0700

    Updating Selenium version.

--- grid/standalone.sh
+++ grid/standalone.sh
@@ -2,5 +2,4 @@
 
 java -Dwebdriver.gecko.driver=./drivers/geckodriver \
     -jar selenium-server-standalone-3.13.0.jar \
-    -role standalone \
-    -nodeConfig ./nodeConfig.json
+    -role standalone
